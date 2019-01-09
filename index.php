a2d99cbad2f65df08b80c55e8a7c786c5bec97fa
Mellske <blackvq35@gmail.com>
2019-01-09 15:39:18 -0600

    finished markup and styles

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://fonts.googleapis.com/css?family=Asap:400,500" rel="stylesheet">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
     <link rel="stylesheet" href="css/main.css">
     <title>TODO:</title>
   </head>
@@ -15,18 +15,24 @@
 
     <main>
         <h1>TODO:</h1>
-        <ul>
-            <li><span class="item">Make Lunch</span></li>
-            <li><span class="item done">Go to meeting</span></li>
+        <ul class="items">
+            <li><span class="item">Make Lunch</span>
+                <a href="#" id="mark-complete"><i class="fas fa-check"></i></a>
+                <a href="#" id="remove"><i class="fas fa-times"></i></a>
+            </li>
+            <li>
+                <span class="item done">Go to meeting</span>
+            </li>
         </ul>
-        <form class="item-add" action="add.php" method="post">
+        <form class="item-add" action="add.php" method="post" autocomplete="off">
             <input class="item-input" type="text" name="item-name" placeholder="Enter you todo list item here.">
             <input type="submit" value="Add">
         </form>
     </main>
-
-    <footer id="main-footer">
-      Copyright &copy; 2019
-    </footer>
   </body>
 </html>
+
+
+<!--
+    when marked complete remove the check mark button, and put a line through the text
+--> 
